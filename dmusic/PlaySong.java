09d25b4fa4617c437348bd54672521a510e9454f
Devansh Mehra <81522384+Devansh-Mehra@users.noreply.github.com>
2022-05-06 23:25:37 +0530

    Add files via upload

---
+++ dmusic/PlaySong.java
@@ -0,0 +1,153 @@
+package com.example.dmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class PlaySong extends AppCompatActivity {
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        updateSeek.interrupt();
+    }
+
+    TextView textView;
+    ImageView previous,play,next;
+    ArrayList<File> songs;
+    MediaPlayer mediaPlayer;
+    String textContent;
+    int position;
+    SeekBar seekBar;
+    Thread updateSeek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+        textView = findViewById(R.id.textView);
+        previous = findViewById(R.id.previous);
+        play = findViewById(R.id.play);
+        next = findViewById(R.id.next);
+        seekBar = findViewById(R.id.seekBar);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        songs = (ArrayList)bundle.getParcelableArrayList("songList");
+        textContent = intent.getStringExtra("currentSong");                 //to view name of song in textview
+        textView.setText(textContent);
+        textView.setSelected(true);
+        position = intent.getIntExtra("position", 0);
+        Uri uri = Uri.parse(songs.get(position).toString());
+        mediaPlayer = MediaPlayer.create(this,uri);                       //to play the song
+        mediaPlayer.start();
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+        updateSeek = new Thread(){
+            @Override
+            public void run(){
+                int currentPosition = 0;
+                try {
+                    while(currentPosition<mediaPlayer.getDuration()){
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentPosition);
+                        sleep(800);
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        updateSeek.start();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()){        //if mediaplayer is plying
+                    play.setImageResource(R.drawable.play);
+                    mediaPlayer.pause();
+                }
+                else{
+                    play.setImageResource(R.drawable.pause);
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                if(position!=0){
+                    position=position-1;
+                }
+                else{
+                    position=songs.size()-1; // if position is 0 then play last song
+                }
+                Uri uri = Uri.parse(songs.get(position).toString());
+                mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);                       //to play the song
+                mediaPlayer.start();
+                play.setImageResource(R.drawable.pause);
+                seekBar.setMax(mediaPlayer.getDuration());
+                textContent = songs.get(position).getName().toString();
+                textView.setText(textContent);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                if(position!=songs.size()-1){
+                    position=position+1;
+                }
+                else{
+                    position=0;
+                }
+                Uri uri = Uri.parse(songs.get(position).toString());
+                mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);                       //to play the song
+                mediaPlayer.start();
+                play.setImageResource(R.drawable.pause);
+                seekBar.setMax(mediaPlayer.getDuration());
+                textContent = songs.get(position).getName().toString();
+                textView.setText(textContent);
+            }
+        });
+
+
+    }
+}
